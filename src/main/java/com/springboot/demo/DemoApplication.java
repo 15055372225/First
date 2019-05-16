8a680d01fc24848a6f5c2ed7ef67d2fe2729b642
15055372225 <15055372225@163.com>
2019-05-16 09:42:51 +0800

    a4
    Change-Id: I0cb4b43ba14d3b71fffd5b66328a652695269cf5

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
    //main
-    //text
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
